41f9da8b515ca64d09f4d4e737c01582d8a3b493
Andre Lehmann <aisberg@posteo.de>
2017-10-29 18:26:22 +0100

    Add MadiaDB Dockerfile

---
+++ mariadb/provision/scripts/mariadb.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# exit on errors
+set -eo pipefail
+
+print_info "Configuring MariaDB"
+
+# prepare parameter
+case "$(convert_to_lower_case "$CONFIG")" in
+    tiny)
+        export CONFIG=tiny
+        ;;
+    small)
+        export CONFIG=small
+        ;;
+    large)
+        export CONFIG=large
+        ;;
+    huge)
+        export CONFIG=huge
+        ;;
+    custom)
+        export CONFIG=custom
+        ;;
+    medium|*)
+        export CONFIG=medium
+        ;;
+esac
+
+DATADIR=/var/lib/mysql
+
+if [ ! -d "$DATADIR/mysql" ]; then
+    # check if all keys were supplied
+    if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
+        print_error "To initialize MariaDB the 'MYSQL_ROOT_PASSWORD' must be specified!"
+        exit 1
+    fi
+
+    mkdir -p "$DATADIR"
+    chown -R mysql:mysql "$DATADIR"
+
+    print_info 'Initializing database'
+    mysql_install_db --datadir="$DATADIR" --rpm --user=mysql
+    print_info 'Database initialized'
+
+    mysqld --user=mysql --datadir="$DATADIR" --skip-networking &
+    pid="$!"
+
+    mysql=( mysql --protocol=socket -uroot -hlocalhost )
+
+    for i in {30..0}; do
+        if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
+            break
+        fi
+        print_info 'MariaDB init process in progress...'
+        sleep 1
+    done
+    if [ "$i" = 0 ]; then
+        print_error 'MariaDB init process failed.'
+        exit 1
+    fi
+
+    mysql_tzinfo_to_sql /usr/share/zoneinfo | sed 's/Local time zone must be set--see zic manual page/FCTY/' | "${mysql[@]}" mysql
+
+    "${mysql[@]}" <<-EOSQL
+			-- What's done in this file shouldn't be replicated
+			--  or products like mysql-fabric won't work
+			SET @@SESSION.SQL_LOG_BIN=0;
+			DELETE FROM mysql.user ;
+			CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
+			GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
+			DROP DATABASE IF EXISTS test ;
+			FLUSH PRIVILEGES ;
+		EOSQL
+
+    if ! kill -s TERM "$pid" || ! wait "$pid"; then
+        print_error 'MariaDB init process failed.'
+        exit 1
+    fi
+fi
+
+# set owner to mysql
+chown -R mysql:mysql "$DATADIR"
+unset DATADIR
