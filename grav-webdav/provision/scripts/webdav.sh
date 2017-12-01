8a9af2246d1c1f153592755c2c38582d41af6d25
Andre Lehmann <aisberg@posteo.de>
2017-12-01 01:04:02 +0100

    Fix execution of cron job

--- grav-webdav/provision/scripts/webdav.sh
+++ grav-webdav/provision/scripts/webdav.sh
@@ -14,8 +14,11 @@ function val(){
     echo "${!varname}"
 }
 
+mkdir -p /usr/local/etc/cron
 c=1
-while [[ -n "`val WEBDAV_PASSWORD $c`" ]]; do
-    echo "`val WEBDAV_CRON_SCHEDULE $c` nextcloudcmd -u \"`val WEBDAV_USER $c`\" -p \"`val WEBDAV_PASSWORD $c`\" --silent \"`val WEBDAV_LOCAL_PATH $c`\" \"`val WEBDAV_REMOTE_URL $c`\"" | crontab -u www-data -
+while [[ -n "`val WEBDAV_REMOTE_URL $c`" ]]; do
+    echo "`val WEBDAV_CRON_SCHEDULE $c` /bin/bash /usr/local/etc/cron/`printf %02d $c`.sh > /dev/null 2>&1" | crontab -u www-data -
+    # workaround: put nextcloudcmd into bash script and let cron execute that script
+    echo "/usr/bin/nextcloudcmd -u \"`val WEBDAV_USER $c`\" -p \"`val WEBDAV_PASSWORD $c`\" --silent \"`val WEBDAV_LOCAL_PATH $c`\" \"`val WEBDAV_REMOTE_URL $c`\"" > /usr/local/etc/cron/`printf "%02d" $c`.sh
     let "c++"
 done
