088e62db1a759045e74de8bd6e3a5cca76811c67
Andre Lehmann <aisberg@posteo.de>
2017-12-07 23:33:26 +0100

    Install grav with composer

--- grav/provision/scripts/grav.sh
+++ grav/provision/scripts/grav.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# exit on errors
 set -e
 
 print_info "Configuring Grav"
@@ -17,24 +15,18 @@ if [[ `standardise_bool "$CLEAN_INSTALLATION" False` == "True" ]]; then
 fi
 
 # if Grav is not yet installed, copy it into web root
-if [ ! -f "/var/www/grav/index.php" ]; then
+if [ ! -f "/var/www/grav/bin/grav" ]; then
     if ! is_dir_empty /var/www/grav; then
         print_error "Install dir is not empty! Make sure the target dir is empty before trying to install a new Grav!"
         exit 1
     fi
-    print_info "No previous Grav installation found, creating a new one"
+    print_info "Installing Grav, might take a few seconds"
 
-    cd /tmp
-    unzip -qq -d . /usr/local/src/grav.zip
-    shopt -s dotglob
-    mv -f grav-admin/* /var/www/grav/
-    rm -rf /tmp/*
-    shopt -u dotglob
+    chown www-data:www-data /var/www/grav
+    su -s /bin/sh www-data -c "COMPOSER_CACHE_DIR=/tmp/composer-cache composer --no-interaction --quiet create-project getgrav/grav /var/www/grav && cd /var/www/grav && php bin/gpm --no-interaction --quiet install admin"
+    rm -rf /tmp/composer-cache
 
-    # set rightful owner
-    chown -R www-data:www-data /var/www/grav
-
-elif [[ "$AUTO_UPDATE" == "False" ]]; then
+elif [[ "$AUTO_UPDATE" == "True" ]]; then
     pushd /var/www/grav > /dev/null
     su -s /bin/sh www-data -c "php bin/gpm selfupgrade -f"
     popd > /dev/null
