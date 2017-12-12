97681f5fda805ed0c6306ce0d0e07c14c0cdaea1
Andre Lehmann <aisberg@posteo.de>
2017-12-12 14:14:57 +0100

    Chown the phpMyAdmin installation dir directly in Dockerfile

--- phpmyadmin/provision/scripts/phpMyAdmin.post.sh
+++ phpmyadmin/provision/scripts/phpMyAdmin.post.sh
@@ -3,7 +3,6 @@
 unset BLOWFISH_SECRET
 
 # set owner:group and permissions
-chown www-data /var/lib/mysql_dumps
+chown www-data /var/lib/mysql_dumps /var/www/phpMyAdmin/config.inc.php
 chmod 770 /var/lib/mysql_dumps
-chown www-data:www-data -R /var/www/phpMyAdmin
 chmod 660 /var/www/phpMyAdmin/config.inc.php
