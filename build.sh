0ef6aac8021a376f4aa16d6372735819d8e2e96b
Andre Lehmann <aisberg@posteo.de>
2017-12-14 17:14:12 +0100

    Build multiple Dockerfiles

--- build.sh
+++ build.sh
@@ -30,10 +30,14 @@ function print_ok() {
     echo "${C_OK}$1${C_RESET}"
 }
 
+function cleanup() {
+    while popd >/dev/null 2>&1; do : ;done
+    rm -rf "$src_dir/building_image"
+}
+
 function Exit() {
     # $1 = return code
-    while popd >/dev/null 2>&1; do : ;done
-    rm -r "$src_dir/building_image"
+    cleanup
     exit $1
 }
 
@@ -54,7 +58,7 @@ function find_dockerfile() {
 
 cd "$(dirname $0)"
 
-USAGE="Usage: $0 [OPTIONS] DIR_CONTAINING_DOCKERFILE
+USAGE="Usage: $0 [OPTIONS] DIR_CONTAINING_DOCKERFILE...
 
 Build docker image from a Dockerfile.
 
@@ -64,66 +68,73 @@ Build docker image from a Dockerfile.
 no_cache_flag="--no-cache=false"
 src_dir=""
 while [[ $# > 0 ]]; do
-	key="$1"
-	case "$key" in
-		-h|--help)
-			echo "$USAGE"
-			exit 0
-			;;
-		-n|--no-cache)
-			no_cache_flag="--no-cache=true"
-			shift 1
-			;;
-		*)
-            break
-			;;
-	esac
+  	key="$1"
+  	case "$key" in
+    		-h|--help)
+    			echo "$USAGE"
+    			exit 0
+    			;;
+    		-n|--no-cache)
+    			no_cache_flag="--no-cache=true"
+    			shift 1
+    			;;
+    		*)
+                break
+    			;;
+  	esac
 done
-if (( $# != 1 )); then
+if (( $# == 0 )); then
     echo "$USAGE"
     exit 1
 else
     src_dir="$1"
 fi
 
-# get absolute path
-src_dir=`readlink -f "$src_dir"`
+while [[ -n "$1" ]]; do
+    # get absolute path
+    src_dir=`readlink -f "$1"`
 
-# find Dockerfile
-dockerfile=`find_dockerfile "$src_dir"`
-if [ -z "$dockerfile" ]; then
-    print_error "No Dockerfile found in: $src_dir"
-    Exit 1
-fi
+    # find Dockerfile
+    dockerfile=`find_dockerfile "$src_dir"`
+    if [ -z "$dockerfile" ]; then
+        print_error "No Dockerfile found in: $src_dir"
+        Exit 1
+    fi
 
-# get image name and additional tags from the comment in the first line
-file_head="$(head -n 1 $dockerfile)"
-if [[ "$file_head" =~ ^\# ]]; then
-    for image_name in $file_head; do
-        if [[ "$image_name" =~ s*([a-zA-Z0-9\._-]+\/[a-zA-Z0-9\:\._-]+) ]]; then
-            image_names=("${image_names[@]}" "$image_name")
-        fi
-    done
-fi
-unset file_head
-if [[ ${#image_names[@]} == 0 ]]; then
-    print_error "Name for image not defined. The name must be specified in the first line of the 'Dockerfile' as a comment."
-    exit 1
-fi
+    # get image name and additional tags from the comment in the first line
+    file_head="$(head -n 1 $dockerfile)"
+    image_names=()
+    if [[ "$file_head" =~ ^\# ]]; then
+        for image_name in $file_head; do
+            if [[ "$image_name" =~ s*([a-zA-Z0-9\._-]+\/[a-zA-Z0-9\:\._-]+) ]]; then
+                image_names+=("$image_name")
+            fi
+        done
+    fi
+    unset file_head
+    if [[ ${#image_names[@]} == 0 ]]; then
+        print_error "Name for image not defined. The name must be specified in the first line of the 'Dockerfile' as a comment."
+        exit 1
+    fi
 
-bash ./make-release.sh -f "$src_dir" "$src_dir/building_image"
+    bash ./make-release.sh -f "$src_dir" "$src_dir/building_image"
 
-# build Docker image
-print_info "Building image '${image_names[0]}'..."
-pushd "$src_dir/building_image" >/dev/null
-if docker build --rm "$no_cache_flag" -t "${image_names[0]}" . ; then
-    print_ok "Successfully built '${image_names[0]}'"
-    for (( i = 1; i < ${#image_names[@]}; i++ )); do
-        docker tag "${image_names[0]}" "${image_names[$i]}" 1>/dev/null
-        print_ok "Added tag '${image_names[$i]}'"
-    done
-else
-    print_error "Failed to build '${image_names[0]}'"
-    Exit 1
-fi
+    # build Docker image
+    print_info "Building image '${image_names[0]}'..."
+    pushd "$src_dir/building_image" >/dev/null
+    if docker build --rm "$no_cache_flag" -t "${image_names[0]}" . ; then
+        print_ok "Successfully built '${image_names[0]}'"
+        for (( i = 1; i < ${#image_names[@]}; i++ )); do
+            docker tag "${image_names[0]}" "${image_names[$i]}" 1>/dev/null
+            print_ok "Added tag '${image_names[$i]}'"
+        done
+    else
+        print_error "Failed to build '${image_names[0]}'"
+        Exit 1
+    fi
+    cleanup
+
+    shift 1
+done
+
 Exit 0;
