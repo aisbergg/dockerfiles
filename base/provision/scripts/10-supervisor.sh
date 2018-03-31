854e98d6ebf4a3dc8630be3318c8aa7218876d5b
Andre Lehmann <aisberg@posteo.de>
2018-03-31 23:25:12 +0200

    Rework of base image

--- base/provision/scripts/10-supervisor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# configure the supervisor daemon
-
-print_info "Configuring Supervisor"
-
-# if SUPERVISOR_USER and SUPERVISOR_PW are supplied the inet_http_server is added
-if [[ -n "$SUPERVISOR_USER" || -n "$SUPERVISOR_PW" ]]; then
-    print_info "Adding authentication credentials to control supervisor via http connection"
-fi
