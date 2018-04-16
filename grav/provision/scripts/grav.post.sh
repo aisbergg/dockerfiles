1a22bd718c91784b618d0593f8937deb704b9805
Andre Lehmann <aisberg@posteo.de>
2018-04-16 19:20:37 +0200

    Build on new Nginx-PHP7 image

--- grav/provision/scripts/grav.post.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-chmod +x /bin/gpm
--- grav/provision/tasks/grav.sh
+++ grav/provision/tasks/grav.sh
@@ -3,11 +3,11 @@ set -e
 
 print_info "Configuring Grav"
 
-export NGINX_TLS_ONLY=`bool "$NGINX_TLS_ONLY" true`
-export AUTO_UPDATE=`bool "$AUTO_UPDATE" true`
+export NGINX_REWRITE_HTTPS=$(bool "$NGINX_REWRITE_HTTPS" false)
+export AUTO_UPDATE=$(bool "$AUTO_UPDATE" true)
 
 # removing all files before installing
-if [[ `bool "$CLEAN_INSTALLATION" false` == "True" ]]; then
+if [[ $(bool "$CLEAN_INSTALLATION" false) == "True" ]]; then
     print_info "Removing all files in installation dir"
     shopt -s dotglob
     rm -rf /var/www/grav/*
@@ -15,21 +15,22 @@ if [[ `bool "$CLEAN_INSTALLATION" false` == "True" ]]; then
 fi
 
 # if Grav is not yet installed, copy it into web root
-if [ ! -f "/var/www/grav/bin/grav" ]; then
+if [[ ! -f "/var/www/grav/bin/grav" ]]; then
     if ! is_dir_empty /var/www/grav; then
         print_error "Install dir is not empty! Make sure the target dir is empty before trying to install a new Grav!"
         exit 1
     fi
-    print_info "Installing Grav, might take a few seconds"
-
-    chown www-data:www-data /var/www/grav
-    su -s /bin/sh www-data -c "COMPOSER_CACHE_DIR=/tmp/composer-cache composer --no-interaction --quiet create-project getgrav/grav /var/www/grav && cd /var/www/grav && php bin/gpm --no-interaction --quiet install admin"
+    print_info "Installing Grav, might take some time..."
+    COMPOSER_CACHE_DIR=/tmp/composer-cache composer --no-interaction create-project getgrav/grav /var/www/grav
+    pushd /var/www/grav >/dev/null
+    php bin/gpm --no-interaction install admin
+    popd >/dev/null
     rm -rf /tmp/composer-cache
 
 elif [[ "$AUTO_UPDATE" == "True" ]]; then
     pushd /var/www/grav > /dev/null
     set +e
-    su -s /bin/sh www-data -c "php bin/gpm selfupgrade -f"
+    php bin/gpm selfupgrade -f
     set -e
     popd > /dev/null
 fi
