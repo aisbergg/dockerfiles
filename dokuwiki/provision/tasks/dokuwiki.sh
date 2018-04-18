2faec0bd3e5f6f81b423698c3ab9a842a7b021c8
Andre Lehmann <aisberg@posteo.de>
2018-04-18 21:04:15 +0200

    Build on new Nginx-PHP7 image

--- dokuwiki/provision/tasks/dokuwiki.sh
+++ dokuwiki/provision/tasks/dokuwiki.sh
@@ -6,7 +6,7 @@ set -e
 print_info "Configuring DokuWiki"
 
 # removing all files before installing
-if [[ `bool "$CLEAN_INSTALLATION" false` == true ]]; then
+if [[ $(bool "$CLEAN_INSTALLATION" false) == true ]]; then
     print_info "Removing all files in installation dir"
     shopt -s dotglob
     rm -rf /var/www/dokuwiki/*
@@ -14,7 +14,7 @@ if [[ `bool "$CLEAN_INSTALLATION" false` == true ]]; then
 fi
 
 # if DokuWiki is not yet installed, copy it into web root
-if [ ! -f '/var/www/dokuwiki/doku.php' ]; then
+if [[ ! -f '/var/www/dokuwiki/doku.php' ]]; then
     print_info "No previous Dokuwiki installation found, creating a new one"
     if ! is_dir_empty /var/www/dokuwiki; then
         print_error "Install dir is not empty! Make sure the target dir is empty before trying to install a new DokuWiki!"
@@ -24,22 +24,16 @@ if [ ! -f '/var/www/dokuwiki/doku.php' ]; then
     # extract DokuWiki files
     tar -xzf /usr/local/src/dokuwiki.tgz -C /var/www/dokuwiki --strip-components=1
 
-    # setting owner and group to 'www-data'
-    chown -R www-data:www-data /var/www/dokuwiki
-
 # check if the installed version can be upgraded
-elif [[ `bool "$AUTO_UPDATE"` == "True" ]]; then
+elif [[ $(bool "$AUTO_UPDATE") == "True" ]]; then
     # information about upgrading dokuwiki can be found here: https://www.dokuwiki.org/install:upgrade
     INSTALLED_VERSION="$(head -n 1 /var/www/dokuwiki/VERSION | grep -o '^[^\ ]*')"
-    if [ "$INSTALLED_VERSION" != "$DOKUWIKI_VERSION" ]; then
+    if version_greater "$DOKUWIKI_VERSION" "$INSTALLED_VERSION" ; then
         # upgrade installation without destroying the userdata
         print_info "Upgrading DokuWiki installation from $INSTALLED_VERSION to ${DOKUWIKI_VERSION}"
         cd /var/www/dokuwiki
         tar -xzf /usr/local/src/dokuwiki.tgz --strip-components=1
         grep -Ev '^($|#)' data/deleted.files | xargs -n 1 rm -vrf
-
-        # set file owner to www-data but leave the group as it is
-        chown -R www-data /var/www/dokuwiki
     fi
 
     unset INSTALLED_VERSION
