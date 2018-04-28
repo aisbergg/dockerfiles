fcec08544e92a4a04a69167641662346ce086fdd
Andre Lehmann <aisberg@posteo.de>
2018-04-28 17:10:38 +0200

    Set PHP timezone same as server timezone

--- nginx-php5/provision/tasks/20-php.sh
+++ nginx-php5/provision/tasks/20-php.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 print_info "Configuring PHP5"
+
+# export timezone variable to be used in PHP date/time functions
+export TZ=$(php -r 'echo timezone_name_from_abbr(exec("date +%Z"));')
--- nginx-php7/provision/tasks/20-php.sh
+++ nginx-php7/provision/tasks/20-php.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 print_info "Configuring PHP7"
+
+# export timezone variable to be used in PHP date/time functions
+export TZ=$(php -r 'echo timezone_name_from_abbr(exec("date +%Z"));')
