af8432ed852428e616b458fc6ba89f69a3a5ffb4
Andre Lehmann <aisberg@posteo.de>
2018-04-28 17:38:14 +0200

    Make concrete5 work again

---
+++ concrete5/provision/post_tasks/30-concrete5.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+if [[ -f /data/www/.need-to-install ]]; then
+    CONCRETE5_DB_SERVER=${CONCRETE5_DB_SERVER:-"mysql"}
+    CONCRETE5_DB_USERNAME=${CONCRETE5_DB_USERNAME:-"concrete5"}
+    CONCRETE5_DB_PASSWORD=${CONCRETE5_DB_PASSWORD:-""}
+    CONCRETE5_DB_NAME=${CONCRETE5_DB_NAME:-""}
+    CONCRETE5_SITE_NAME=${CONCRETE5_SITE_NAME:-"Concrete5 Site"}
+    CONCRETE5_STARTING_POINT=${CONCRETE5_STARTING_POINT:-"elemental_blank"}
+    CONCRETE5_EMAIL=${CONCRETE5_EMAIL:-""}
+    CONCRETE5_PASSWORD=${CONCRETE5_PASSWORD:-""}
+    CONCRETE5_LOCALE=${CONCRETE5_LOCALE:-"en_US"}
+
+    print_info "Wait a few seconds for the database to become available"
+    sleep 10
+
+    pushd /data/www >/dev/null
+    php concrete/bin/concrete5 c5:install --db-server="$CONCRETE5_DB_SERVER" --db-username="$CONCRETE5_DB_USERNAME" --db-password="$CONCRETE5_DB_PASSWORD" --db-database="$CONCRETE5_DB_NAME" --site="$CONCRETE5_SITE_NAME" --starting-point="$CONCRETE5_STARTING_POINT" --admin-email="$CONCRETE5_EMAIL" --admin-password="$CONCRETE5_PASSWORD" --site-locale="$CONCRETE5_LOCALE"
+    popd >/dev/null
+
+    rm /data/www/.need-to-install
+fi
--- concrete5/provision/tasks/30-concrete5.sh
+++ concrete5/provision/tasks/30-concrete5.sh
@@ -7,19 +7,23 @@ print_info "Configuring Concrete5"
 if [[ $(bool "$CLEAN_INSTALLATION" false) == true ]]; then
     print_info "Removing all files in installation dir"
     shopt -s dotglob
-    rm -rf /var/www/concrete5/*
+    rm -rf /data/www/*
     shopt -u dotglob
 fi
 
 # check if an valid Concrete5 installation is present, else create a new one
-if [ ! -f /var/www/concrete5/concrete/config/concrete.php ]; then
+if [[ ! -f /data/www/concrete/config/concrete.php ]]; then
     print_info "No previous Concrete5 installation found, creating a new one"
-    if ! is_dir_empty /var/www/concrete5; then
+    if ! is_dir_empty /data/www; then
         print_error "Install dir is not empty! Make sure the target dir is empty before trying to install a new Concrete5!"
         exit 1
     fi
-    unzip_strip /usr/local/src/concrete5.zip /var/www/concrete5/
-elif [[ -f /var/www/concrete5/application/config/database.php ]]; then
-    chmod o-rwx /var/www/concrete5/application/config/database.php
+
+    unzip_strip /usr/local/src/concrete5.zip /data/www/
+    echo "Still need to install" > /data/www/.need-to-install
+
+elif [[ -f /data/www/application/config/database.php ]]; then
+    chmod o-rwx /data/www/application/config/database.php
 fi
+
 # to upgrade the installation checkout: http://documentation.concrete5.org/developers/installation/upgrading-concrete5
