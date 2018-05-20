8c3d81b61ce51eb4a6f931ad8baae8354316866d
Andre Lehmann <aisberg@posteo.de>
2018-05-20 21:42:13 +0200

    Fix path

--- nextcloud/provision/post_tasks/30-nextcloud.sh
+++ nextcloud/provision/post_tasks/30-nextcloud.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-if [[ -f /var/www/nextcloud/.needs-upgrade ]]; then
+if [[ -f /data/www/.needs-upgrade ]]; then
     # call upgrade routine
     cd /data/www
     php occ upgrade --no-app-disable
 
     # remove upgrade indicator if upgrade succeeded
-    rm /var/www/nextcloud/.needs-upgrade
+    rm /data/www/.needs-upgrade
 fi
