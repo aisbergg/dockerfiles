3c8ce55dfbe4e60b743393d660f34ec3ebff8f19
Andre Lehmann <aisberg@posteo.de>
2018-09-19 16:43:16 +0200

    Use customized version of official Docker image

---
+++ haproxy/README.md
@@ -0,0 +1,2 @@
+# HAProxy
+This is a fork of the [official HAProxy Docker image](https://hub.docker.com/_/haproxy/). The difference is that this version is compiled against LibreSSL instead of OpenSSL.
--- haproxy/release/provision/post_tasks/10-haproxy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-cat /usr/local/src/haproxy.cfg.template.part1 /usr/local/src/haproxy.cfg.template.part2 > /container/cfg/haproxy.cfg.template
-
-if [[ ! -f /container/cfg/haproxy.cfg ]]; then
-    cp /usr/local/src/empty-haproxy.cfg /container/cfg/haproxy.cfg
-fi
