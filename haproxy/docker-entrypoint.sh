8dc43902cb35c030f847c7e680260a25d2b5f052
Andre Lehmann <aisberg@posteo.de>
2018-11-01 09:17:47 +0100

    Bump HAProxy version up to 1.8.14

--- haproxy/docker-entrypoint.sh
+++ haproxy/docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
-	set -- tini -- haproxy "$@"
+	set -- haproxy "$@"
 fi
 
 if [ "$1" = 'haproxy' ]; then
@@ -11,7 +11,7 @@ if [ "$1" = 'haproxy' ]; then
 	# if the user wants "haproxy", let's add a couple useful flags
 	#   -W  -- "master-worker mode" (similar to the old "haproxy-systemd-wrapper"; allows for reload via "SIGUSR2")
 	#   -db -- disables background mode
-	set -- tini -- haproxy -W -db -p /container/run/haproxy.pid "$@"
+	set -- haproxy -W -db -p /container/run/haproxy.pid "$@"
 fi
 
 exec "$@"
