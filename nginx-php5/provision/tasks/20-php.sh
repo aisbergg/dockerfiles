338220fc6dbce96f3dae8cce6fcede7726a53f7b
Andre Lehmann <aisberg@posteo.de>
2019-02-02 13:56:05 +0100

    Remove nginx-php5 Dockerfile

--- nginx-php5/provision/tasks/20-php.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-print_info "Configuring PHP5"
-
-# export timezone variable to be used in PHP date/time functions
-export TZ=$(php -r 'echo timezone_name_from_abbr(exec("date +%Z"));')
