26054d03b9ebdfcc0acd3cd18e9a77943a1bee43
Andre Lehmann <aisberg@posteo.de>
2019-03-06 11:57:05 +0100

    Use WP-CLI to install and update Wordpress

--- wordpress/provision/tasks/30-wordpress.sh
+++ wordpress/provision/tasks/30-wordpress.sh
@@ -12,10 +12,28 @@ if [[ $(bool "$CLEAN_INSTALLATION" false) == "true" ]]; then
 fi
 
 # if Wordpress is not yet installed, copy it into web root
-if [[ ! -d /container/www/wp-content || -f /container/www/.installation-in-progess ]]; then
-    print_info "No previous Wordpress installation found, creating a new one"
-    if ! is_dir_empty /container/www || [[ -f /container/www/.installation-in-progess ]]; then
-        print_error "Install dir is not empty! Make sure the target dir is empty before trying to install Wordpress!"
+if [[ ! -f /container/www/wp-load.php || -f /container/www/.installation-in-progess ]]; then
+    print_info "Installing Wordpress"
+
+    WORDPRESS_LOCALE="${WORDPRESS_LOCALE:-de_DE}"
+    WORDPRESS_DB_PREFIX="${WORDPRESS_DB_PREFIX:-wp_}"
+    WORDPRESS_TITLE="${WORDPRESS_TITLE:-"Wordpress in a Docker Container"}"
+
+    if [[ -z "$WORDPRESS_DB_NAME" || -z "$WORDPRESS_DB_USER" || -z "$WORDPRESS_DB_PASS" || -z "$WORDPRESS_DB_HOST" ]]; then
+        print_error "In order to install Wordpress you need to provide information about the database connection: WORDPRESS_DB_NAME, WORDPRESS_DB_USER, WORDPRESS_DB_PASS, WORDPRESS_DB_HOST"
+        exit 1
+    fi
+
+    if [[ -z "$WORDPRESS_URL" ]]; then
+        if [[ -z "$DOMAINNAMES" ]]; then
+            print_error "In order to install Wordpress you need to provide a URL that : WORDPRESS_DB_NAME, WORDPRESS_DB_USER, WORDPRESS_DB_PASS, WORDPRESS_DB_HOST"
+            exit 1
+        fi
+        WORDPRESS_URL="https://${DOMAINNAMES%%,*}"
+    fi
+
+    if [[ -z "$WORDPRESS_ADMIN_USER" || -z "$WORDPRESS_ADMIN_PASSWORD" || -z "$WORDPRESS_ADMIN_EMAIL" ]]; then
+        print_error "In order to install Wordpress you need to provide information about the admin user: WORDPRESS_ADMIN_USER, WORDPRESS_ADMIN_PASSWORD, WORDPRESS_ADMIN_EMAIL"
         exit 1
     fi
 
@@ -23,7 +41,30 @@ if [[ ! -d /container/www/wp-content || -f /container/www/.installation-in-proge
     # create lockfile
     touch .installation-in-progess
 
-    tar xzf /usr/local/src/wordpress.tar.gz --strip-components=1
+    # download Wordpress source
+    wp-cli.phar core download --allow-root --no-color --locale="$WORDPRESS_LOCALE"
+    # create configuration file
+    wp-cli.phar config create \
+        --allow-root --no-color \
+        --dbname="$WORDPRESS_DB_NAME" \
+        --dbuser="$WORDPRESS_DB_USER" \
+        --dbpass="$WORDPRESS_DB_PASS" \
+        --dbhost="$WORDPRESS_DB_HOST" \
+        --dbprefix="$WORDPRESS_DB_PREFIX" \
+        --locale="$WORDPRESS_LOCALE" \
+        --url="$WORDPRESS_URL"
+    # create database tables
+    wp-cli.phar core install \
+        --allow-root --no-color \
+        --url="$WORDPRESS_URL" \
+        --title="$WORDPRESS_TITLE" \
+        --admin_user="$WORDPRESS_ADMIN_USER" \
+        --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+        --admin_email="$WORDPRESS_ADMIN_EMAIL" \
+        --skip-email
+
+    mkdir -p wp-content/{themes,plugins,users,uploads}
+
     shopt -s dotglob
     chmod g+rwX,o-rwx -R ./* &&\
     chgrp root -R ./*
@@ -32,7 +73,12 @@ if [[ ! -d /container/www/wp-content || -f /container/www/.installation-in-proge
     rm .installation-in-progess
     popd >/dev/null
 
-    # information about upgrading wordpress can be found here: https://codex.wordpress.org/Updating_WordPress
+elif [[ $(bool "$WORDPRESS_AUTO_UPDATE" false) == "true" ]]; then
+    print_info "Updating Wordpress"
+
+    pushd /container/www >/dev/null
+    wp-cli.phar core update --allow-root --no-color
+    popd >/dev/null
 fi
 
 # try to remove permissions for 'other'
@@ -42,5 +88,3 @@ chmod o-rwx /container/www/wp-config.php || true
 if [[ -f /container/www/wp-config.php && "$(stat -c '%a' /container/www/wp-config.php | cut -c 3)" -ge 4 ]]; then
     print_warning "ATTENTION: The settings file 'wp-config.php' should not be world readable. Use 'chmod' to change its permissions."
 fi
-
-export IMAGEMAGICK_SHARED_SECRET="$(create_random_string 64)"
