1f67808257e2395792fc2f2e4526c71b48c7acc2
Andre Lehmann <aisberg@posteo.de>
2020-08-10 19:33:05 +0200

    Rename var 'NGINX_TLS_TERMINATED' to 'NGINX_TLS_ENABLED'

--- nginx/provision/tasks/10-nginx.sh
+++ nginx/provision/tasks/10-nginx.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 print_info "Configuring Nginx"
 
 export NGINX_REWRITE_HTTPS=$(bool "$NGINX_REWRITE_HTTPS" true)
-export NGINX_TLS_TERMINATED=$(bool "$NGINX_TLS_TERMINATED" true)
+export NGINX_TLS_ENABLED=$(bool "$NGINX_TLS_ENABLED" false)
 export NGINX_TLS_CERT=${NGINX_TLS_CERT:-"/container/tls/cert.pem"}
 export NGINX_TLS_KEY=${NGINX_TLS_KEY:-"/container/tls/key.pem"}
 export NGINX_TLS_DHPARAM=${NGINX_TLS_DHPARAM:-"/container/tls/dhparam.pem"}
@@ -12,11 +12,11 @@ export NGINX_TLS_DHPARAM=${NGINX_TLS_DHPARAM:-"/container/tls/dhparam.pem"}
 # !!! IN PRODUCTION USE 4096 !!!
 export NGINX_TLS_DHPARAM_SIZE=${NGINX_TLS_DHPARAM_SIZE:-"512"}
 
-if [[ "$NGINX_TLS_TERMINATED" == false ]]; then
+if [[ "$NGINX_TLS_ENABLED" == true ]]; then
     # TLS certificate and key
     if [[ ! -f "$NGINX_TLS_CERT" && ! -f "$NGINX_TLS_KEY" ]]; then
         print_info "Generating TLS-Key and TLS-Certificate..."
-        openssl req -x509 -newkey rsa:4096 \
+        libressl req -x509 -newkey rsa:4096 \
         -subj "/C=/ST=/L=/O=/CN=$HOSTNAME" \
         -keyout "$NGINX_TLS_KEY" \
         -out "$NGINX_TLS_CERT" \
@@ -35,7 +35,7 @@ if [[ "$NGINX_TLS_TERMINATED" == false ]]; then
     # Diffie-Hellman parameter for forward secrecy
     if [[ ! -f "$NGINX_TLS_DHPARAM" ]]; then
         print_info "Generating ${NGINX_TLS_DHPARAM_SIZE}bit Diffie-Hellman-Parameter (May take a long time)..."
-        openssl dhparam -out "$NGINX_TLS_DHPARAM" "$NGINX_TLS_DHPARAM_SIZE"
+        libressl dhparam -out "$NGINX_TLS_DHPARAM" "$NGINX_TLS_DHPARAM_SIZE"
         chmod o-rwx "$NGINX_TLS_DHPARAM"
     fi
 fi
