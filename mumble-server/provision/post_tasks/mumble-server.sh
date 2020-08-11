770413e6589fcd7c656a6b85dc999bdf3c9af030
Andre Lehmann <aisberg@posteo.de>
2020-08-11 22:42:42 +0200

    Ignore error setting the superuser pw

--- mumble-server/provision/post_tasks/mumble-server.sh
+++ mumble-server/provision/post_tasks/mumble-server.sh
@@ -3,5 +3,5 @@ set -eo pipefail
 
 if [[ -n "$MUMBLE_SUPERUSER_PASSWORD" ]]; then
     # set SuperUser password
-    /usr/bin/murmurd -ini /container/mumble-server/mumble-server.ini -supw "$MUMBLE_SUPERUSER_PASSWORD"
+    /usr/bin/murmurd -ini /container/mumble-server/mumble-server.ini -supw "$MUMBLE_SUPERUSER_PASSWORD" || true
 fi
