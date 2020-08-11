1482f1f358ef38640ddcc20f429380b880929176
Andre Lehmann <aisberg@posteo.de>
2020-08-11 22:29:48 +0200

    Fix TLS settings

--- mumble-server/provision/tasks/mumble-server.sh
+++ mumble-server/provision/tasks/mumble-server.sh
@@ -3,26 +3,18 @@ set -eo pipefail
 
 print_info "Configuring Mumble-Server (Murmur)"
 
-export MUMBLE_TLS_CERT=${MUMBLE_TLS_CERT:-"/container/tls/cert.pem"}
-export MUMBLE_TLS_KEY=${MUMBLE_TLS_KEY:-"/container/tls/key.pem"}
-export MUMBLE_TLS_DHPARAMS=${MUMBLE_TLS_DHPARAMS:-"/container/tls/dhparam.pem"}
+export MUMBLE_SSL_CERT=${MUMBLE_SSL_CERT:-"/container/tls/cert.pem"}
+export MUMBLE_SSL_KEY=${MUMBLE_SSL_KEY:-"/container/tls/key.pem"}
+export MUMBLE_SSL_DHPARAMS=${MUMBLE_SSL_DHPARAMS:-"/container/tls/dhparam.pem"}
 
 # if no TLS certificate is specified, then generate a self-signed one
-if [[ ! -f "$MUMBLE_TLS_CERT" && ! -f "$MUMBLE_TLS_KEY" ]]; then
+if [[ ! -f "$MUMBLE_SSL_CERT" && ! -f "$MUMBLE_SSL_KEY" ]]; then
     print_info "Generating TLS-Key and TLS-Certificate..."
     openssl req -x509 -newkey rsa:4096 \
     -subj "/C=/ST=/L=/O=/CN=Murmur" \
-    -keyout "$MUMBLE_TLS_KEY" \
-    -out "$MUMBLE_TLS_CERT" \
+    -keyout "$MUMBLE_SSL_KEY" \
+    -out "$MUMBLE_SSL_CERT" \
     -days 3650 -nodes -sha256 > /dev/null
 else
     print_info "Using existing TLS key and certificate"
-fi
-# if no Diffie-Hellman parameters file for forward secrecy is specified, then generate it
-if [[ ! -f "$MUMBLE_TLS_DHPARAMS" ]]; then
-    MUMBLE_TLS_DHSIZE=${MUMBLE_TLS_DHSIZE:-2048}
-    print_info "Generating ${MUMBLE_TLS_DHSIZE} bit Diffie-Hellman-Parameter (May take a long time)..."
-    openssl dhparam -out "$MUMBLE_TLS_DHPARAMS" $MUMBLE_TLS_DHSIZE > /dev/null
-else
-    print_info "Using existing dhparams file"
 fi
