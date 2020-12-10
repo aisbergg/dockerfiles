97d854d438618ade9544ea49687674fb18f000ac
Andre Lehmann <aisberg@posteo.de>
2020-12-10 18:17:51 +0100

    Update Nginx-PHP Image

--- nginx-php7/provision/tasks/20-php.sh
+++ nginx-php7/provision/tasks/20-php.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 print_info "Configuring PHP7"
-
-# export timezone variable to be used in PHP date/time functions
-export TZ=$(php -r 'echo timezone_name_from_abbr(exec("date +%Z"));')
